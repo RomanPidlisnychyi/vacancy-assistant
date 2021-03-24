9377ebb665e8826e325dcae829ad9f47541ec735
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-24 08:16:55 +0200

    hover

--- src/components/Lists/VacansiesList/VacancyItem/BtnControl/BtnControl.js
+++ src/components/Lists/VacansiesList/VacancyItem/BtnControl/BtnControl.js
@@ -1,24 +1,14 @@
-import { Button } from 'react-bootstrap';
 import { IconBtn } from './IconBtn';
-import { inputs } from '../../../../../inputs';
 import styles from './BtnControl.module.css';
 
-export default function BtnControl() {
-  const myBtn = inputs.filter(
-    input =>
-      input.name !== 'name' &&
-      input.name !== 'email' &&
-      input.name !== 'password' &&
-      input.name !== 'confirmPassword'
-  );
-
+export default function BtnControl({ myBtn }) {
   const handleBtn = name => {
     console.log('name', name);
   };
   return (
     <div className={styles.container}>
-      {myBtn.map(btn => (
-        <IconBtn key={btn.name} handleBtn={handleBtn} name={btn.name} />
+      {myBtn.map(name => (
+        <IconBtn key={name} handleBtn={handleBtn} name={name} />
       ))}
     </div>
   );
--- src/components/Lists/VacansiesList/VacancyItem/VacancyItem.js
+++ src/components/Lists/VacansiesList/VacancyItem/VacancyItem.js
@@ -4,13 +4,42 @@ import { BtnControl } from './BtnControl';
 import { IconBtn } from './BtnControl/IconBtn';
 import { getVacancy } from '../../../../store/selectors/vacancySelectors';
 import { onDeleteVacancy } from '../../../../store/operations/vacancyOperations';
+import { inputs } from '../../../../inputs';
 import styles from './VacancyItem.module.css';
 
 export default function VacancyItem({ id }) {
   const dispatch = useDispatch();
   const vacancy = useSelector(state => getVacancy(state, id));
 
-  const { date, companyName, URL, status } = vacancy;
+  // const myBtnInCard = inputs.filter(
+  //   input =>
+  //     input.name !== 'name' &&
+  //     input.name !== 'email' &&
+  //     input.name !== 'password' &&
+  //     input.name !== 'confirmPassword'
+  // );
+
+  const {
+    companyName,
+    URL,
+    date,
+    status,
+    position,
+    stack,
+    location,
+    phone,
+  } = vacancy;
+
+  const myBtnInCard = [
+    'companyName',
+    'URL',
+    'phone',
+    'position',
+    'stack',
+    'location',
+  ];
+
+  const myBtnInHeader = [status, 'delete'];
 
   const handleBtn = () => {
     dispatch(onDeleteVacancy(id));
@@ -28,12 +57,12 @@ export default function VacancyItem({ id }) {
             >
               {date}
               {companyName}
-              <IconBtn name={'delete'} handleBtn={handleBtn} />
             </Accordion.Toggle>
+            <BtnControl myBtn={myBtnInHeader} />{' '}
           </Card.Header>
           <Accordion.Collapse eventKey="0">
             <Card.Body className={styles.cardBody}>
-              <BtnControl />
+              <BtnControl myBtn={myBtnInCard} />
             </Card.Body>
           </Accordion.Collapse>
         </Card>
